780f7b2bc085433d23d819c0cce5f85f80263392
luyanggeorge <luyanggeorge@163.com>
2024-11-03 16:28:59 +0000

    Add smooth Riemann problem to explore CFL

--- FV/LAE_Godunov.py
+++ FV/LAE_Godunov.py
@@ -1,8 +1,12 @@
 # Solve the linear advection equation d_t u + a d_x u = 0 (a>0) in [x0,x1] using Godunov method
 # Initial conditions:
 # 	(a) Riemann problem u0(x) = ul,  x < x_mid; ur,  x >=x_mid
-# 	(b) traveling wave u0(x) = sin(2*pi*x/Lx)
+#   (b) Smooth Riemann problem using tanh() [explore the effect of CFL]
+# 	(c) traveling wave u0(x) = sin(2*pi*x/Lx)
 # Boundary condition: specify upwind boundary value u(x0,t) for t>0
+# Visualisation of results:
+# - Discrete exact solutions are plotted at grid points;
+# - Numerical solutions are plotted in solid lines.
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -29,20 +33,27 @@ CFL = 1.0 # in [0,1]
 dt = CFL*(dx/a) # time step
 
 # === setting initial condition ===
-IC = "a"
+IC = "b"
 
 if IC == "a":
-	u0_l=5
-	u0_r=2
+	u0_l = 5
+	u0_r = 2
 	x_mid=(x0+x1)*0.5-1e-8 # shift a bit to avoid the ambiguity at the discontinous point
 	u0 = np.where(x_k < x_mid, u0_l, u0_r) # straightforward
 	#u0 = u0_l*np.heaviside(x_mid-x_k,0) + u0_r*np.heaviside(x_k-x_mid,1) # tricky at x=x_mid, not recommended
 elif IC == "b":
+	x_mid=(x0+x1) * 0.5
+	u0_l = 5
+	u0_r = 2
+	u0_mid = (u0_l+u0_r) * 0.5
+	x_zoom = 20.0
+	u0 = ((u0_r - u0_l)*0.5) * np.tanh( x_zoom *(x_k-x_mid) ) + u0_mid
+elif IC == "c":
 	u0 = np.sin(2*np.pi*x_k/Lx)
 
 # plot IC
 fig, ax = plt.subplots(figsize=(8, 5), layout='constrained')
-ax.plot(x_k, u0, c='k', label=r'$u_0(x)$')
+ax.scatter(x_k, u0, c='k', label=r'$u_0(x)$')
 
 
 t = 0.0 # start time
@@ -56,9 +67,9 @@ output_freq = int(output_dt/dt)
 # === time marching ===
 while t < Tend-0.5*dt:
 	# upwind boundary condition: specify inflow at x=x0
-	if IC=="a":
+	if IC=="a" or IC == "b":
 		u[0]=u0_l
-	elif IC=="b":
+	elif IC=="c":
 		u[0]=np.sin(2*np.pi*x0/Lx-(2*np.pi*a/Lx)*(t+dt))
 
 	for k in range(1,Nx+1):
@@ -74,20 +85,21 @@ while t < Tend-0.5*dt:
 	# plot intermediate results
 	if step % output_freq == 0:
 		print("t=",t)
-		if IC=="a":
+		if IC == "a":
 			#ut = u0_l*np.heaviside(x_mid-(x_k-a*t),0) + u0_r*np.heaviside((x_k-a*t)-x_mid,1) ! tricky at x=x_mid+a*t
 			ut = np.where(x_k-a*t < x_mid, u0_l, u0_r)
-		elif IC=="b":
+		elif IC == "b":
+			ut = ((u0_r - u0_l)*0.5) * np.tanh( x_zoom *(x_k-a*t-x_mid) ) + u0_mid
+		elif IC == "c":
 			ut = np.sin(2*np.pi*x_k/Lx-(2*np.pi*a/Lx)*t)
-		ax.plot(x_k, ut, label=fr'$u_{{ex}}(x,t={t:.3f})$')
-		ax.plot(x_k, u, ls='--', label=fr'$u_{{num}}(x,t={t:.3f})$')
+		ax.scatter(x_k, ut, marker='o', label=fr'$u_{{ex}}(x,t={t:.3f})$') # exact solution
+		ax.plot(x_k, u, ls='-', label=fr'$u_{{num}}(x,t={t:.3f})$') # numerical solution
 
 ax.set_title(r'Finite Volumn Solution of the Linear Advection Equation $\partial_t u + a \partial_x u = 0 \, (a > 0)$')
 ax.set_xlabel(r'$x$')
 ax.set_ylabel(r'$u(x,t)$')
 ax.grid()
-#ax.set_title("")
 ax.legend()          
-plt.show()   
+plt.show()
 
 
